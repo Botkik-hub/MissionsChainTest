58a5460e1e98ef9c13bf516647f9abec3b689a04
Stas Voronov <stasv2003@gmail.com>
2025-06-18 20:34:15 -0400

    Readme created

--- Assets/Scripts/Missions/IMission.cs
+++ Assets/Scripts/Missions/IMission.cs
@@ -8,7 +8,7 @@ public interface IMission : IDisposable
     // I have also added a way to display those missions in ui 
     // Missions known what chain they are belong to
     // Missions Implements IDisposable to clean up after its done or Game closing
-    // I went with using scriptable objects to implement IMissions and they cannot clean up themselves 
+    // I went with using scriptable objects to implement IMissions and because of that I need 
     string MissionName { get; }
     string MissionProgress { get; }
     bool IsCompleted { get; }
